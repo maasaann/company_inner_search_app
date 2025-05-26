ba563f9cebc29c315055e3e57bd00392fd766ad9
maasaann <meyutana@gmail.com>
2025-05-26 17:50:18 +0900

    2025-05-26 17:35

--- initialize.py
+++ initialize.py
@@ -17,11 +17,10 @@ from docx import Document
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
+#from langchain_community.vectorstores import Chroma
 import constants as ct
 
-from chromadb.config import Settings
-
+from langchain_community.vectorstores import FAISS
 
 ############################################################
 # 設定関連
@@ -133,29 +132,11 @@ def initialize_retriever():
     # チャンク分割を実施
     splitted_docs = text_splitter.split_documents(docs_all)
 
-    # # ベクターストアの作成
-    # db = Chroma.from_documents(splitted_docs, embedding=embeddings)
+    # ベクターストアの作成
+    db = FAISS.from_documents(splitted_docs, embedding=embeddings)
 
-    # # ベクターストアを検索するRetrieverの作成
-    # st.session_state.retriever = db.as_retriever(search_kwargs={"k": ct.DOCS_COUNT}) # 【問題1】【問題2】
-    # ✅ ここでクライアント設定を追加
-
-    client_settings = Settings(
-        persist_directory="./chroma_db",  # Streamlit Cloudでも使える一時フォルダ
-        anonymized_telemetry=False
-    )
-
-    # ✅ 設定を渡して Chroma を作成
-    db = Chroma.from_documents(
-        splitted_docs,
-        embedding=embeddings,
-        client_settings=client_settings
-    )
-
-    st.session_state.retriever = db.as_retriever(search_kwargs={"k": ct.DOCS_COUNT})
-
-
-
+    # ベクターストアを検索するRetrieverの作成
+    st.session_state.retriever = db.as_retriever(search_kwargs={"k": ct.DOCS_COUNT}) # 【問題1】【問題2】
 
 
 def initialize_session_state():
